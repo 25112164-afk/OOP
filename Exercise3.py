d4bf5e5d3c44a110013efa80fd374eb34af2e6cf
Kien Nguyen <25112164@st.vju.ac.vn>
2026-03-23 13:19:44 +0700

    Create Exercise3.py

---
+++ Exercise3.py
@@ -0,0 +1,53 @@
+#1 
+def draw_grid_2x2():
+    hang_ngang = '+ - - - - + - - - - +'
+    hang_doc =   '|         |         |'
+
+    print(hang_ngang)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    
+    print(hang_ngang)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    
+    print(hang_ngang)
+print("1. Grid 2x2:")
+draw_grid_2x2()
+print("\n" + "-" * 40 + "\n")
+#2
+def draw_grid_4x4():
+    hang_ngang = '+ - - - - + - - - - + - - - - + - - - - +'
+    hang_doc =   '|         |         |         |         |'
+
+    print(hang_ngang)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    
+    print(hang_ngang)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    
+    print(hang_ngang)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    
+    print(hang_ngang)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    print(hang_doc)
+    
+    print(hang_ngang)
+print("2. Grid 4x4:")
+draw_grid_4x4()
